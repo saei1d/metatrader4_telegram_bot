cf392f65dea624fa150a6c688e77eacfce1c35a0
saei1d <hehehe3bar@gmail.com>
2024-04-10 17:55:37 +0330

    bugg

--- bottons.py
+++ bottons.py
@@ -4,47 +4,92 @@ from telebot import types
 def get_main_buttons():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
     buttons = [
-        types.KeyboardButton("Alpari"),
-        types.KeyboardButton("Forex4u"),
-        types.KeyboardButton("roboforex"),
+        types.KeyboardButton("AlpariMT4"),
+        types.KeyboardButton("Forex4uMT4"),
+        types.KeyboardButton("roboforexMT4"),
+        types.KeyboardButton("AlpariMT5"),
+        types.KeyboardButton("Forex4uMT5"),
+        types.KeyboardButton("roboforexMT5"),
 
     ]
     markup.add(*buttons)
     return markup
 
 
-def balance_robo():
+def balance_robo4():
     markup = types.InlineKeyboardMarkup(row_width=1)
     buttons = [
-        types.InlineKeyboardButton("5k", callback_data="robo5k"),
-        types.InlineKeyboardButton("10k", callback_data="robo10k"),
-        types.InlineKeyboardButton("15k", callback_data="robo15k"),
-        types.InlineKeyboardButton("25k", callback_data="robo25k"),
+        types.InlineKeyboardButton("5k", callback_data="roboMT45k"),
+        types.InlineKeyboardButton("10k", callback_data="roboMT410k"),
+        types.InlineKeyboardButton("15k", callback_data="roboMT415k"),
+        types.InlineKeyboardButton("25k", callback_data="roboMT425k"),
     ]
     markup.add(*buttons)
     return markup
 
 
-def balance_alpari():
+def balance_alpari4():
     markup = types.InlineKeyboardMarkup(row_width=1)
     buttons = [
-        types.InlineKeyboardButton("5k", callback_data="alpari5k"),
-        types.InlineKeyboardButton("10k", callback_data="alpari10k"),
-        types.InlineKeyboardButton("15k", callback_data="alpari15k"),
-        types.InlineKeyboardButton("25k", callback_data="alpari25k"),
+        types.InlineKeyboardButton("5k", callback_data="alpariMT45k"),
+        types.InlineKeyboardButton("10k", callback_data="alpariMT410k"),
+        types.InlineKeyboardButton("15k", callback_data="alpariMT415k"),
+        types.InlineKeyboardButton("25k", callback_data="alpariMT425k"),
     ]
     markup.add(*buttons)
     return markup
 
 
-
-def balance_forex4u():
+def balance_forex4u4():
     markup = types.InlineKeyboardMarkup(row_width=1)
     buttons = [
-        types.InlineKeyboardButton("5k", callback_data="forex4u5k"),
-        types.InlineKeyboardButton("10k", callback_data="forex4u10k"),
-        types.InlineKeyboardButton("15k", callback_data="forex4u15k"),
-        types.InlineKeyboardButton("25k", callback_data="forex4u25k"),
+        types.InlineKeyboardButton("5k", callback_data="forex4uMT45k"),
+        types.InlineKeyboardButton("10k", callback_data="forex4uMT410k"),
+        types.InlineKeyboardButton("15k", callback_data="forex4uMT415k"),
+        types.InlineKeyboardButton("25k", callback_data="forex4uMT425k"),
+    ]
+    markup.add(*buttons)
+    return markup
+
+
+#
+#
+#               👇MT5
+#
+#
+#
+
+def balance_alpari5():
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    buttons = [
+        types.InlineKeyboardButton("5k", callback_data="alpariMT55k"),
+        types.InlineKeyboardButton("10k", callback_data="alpariMT510k"),
+        types.InlineKeyboardButton("15k", callback_data="alpariMT515k"),
+        types.InlineKeyboardButton("25k", callback_data="alpariMT525k"),
+    ]
+    markup.add(*buttons)
+    return markup
+
+
+def balance_forex4u5():
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    buttons = [
+        types.InlineKeyboardButton("5k", callback_data="forex4uMT55k"),
+        types.InlineKeyboardButton("10k", callback_data="forex4uMT510k"),
+        types.InlineKeyboardButton("15k", callback_data="forex4uMT515k"),
+        types.InlineKeyboardButton("25k", callback_data="forex4uMT525k"),
+    ]
+    markup.add(*buttons)
+    return markup
+
+
+def balance_icmarketsMT5():
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    buttons = [
+        types.InlineKeyboardButton("5k", callback_data="icmarketsMT55k"),
+        types.InlineKeyboardButton("10k", callback_data="icmarketsMT510k"),
+        types.InlineKeyboardButton("15k", callback_data="icmarketsMT515k"),
+        types.InlineKeyboardButton("25k", callback_data="icmarketsMT525k"),
     ]
     markup.add(*buttons)
     return markup
--- meta4.py
+++ meta4.py
@@ -13,6 +13,12 @@ def generate_random_code():
     return code
 
 
+def generate_random_codeMT5():
+    code = ''.join(random.choices('0123456789', k=10))
+    return code
+
+
+
 names = [
     "traderone", "tradeone", "tradersone", "onetrader", "onetraderone", "propone", "oneprop", "traderprop",
     "tradersprop", "traderprops"
@@ -29,14 +35,25 @@ def start(message):
 
 @bot.message_handler(func=lambda message: True)
 def forex(message):
-    if message.text == "roboforex":
-        bot.send_message(message.chat.id, "choose your balance", reply_markup=balance_robo())
-    elif message.text == "Alpari":
-        bot.send_message(message.chat.id, "choose your balance", reply_markup=balance_alpari())
-    elif message.text == "Forex4u":
-            bot.send_message(message.chat.id, "choose your balance", reply_markup=balance_forex4u())
+    if message.text == "roboforexMT4":
+        bot.send_message(message.chat.id, "choose your balance", reply_markup=balance_robo4())
+    elif message.text == "AlpariMT4":
+        bot.send_message(message.chat.id, "choose your balance", reply_markup=balance_alpari4())
+    elif message.text == "Forex4uMT4":
+        bot.send_message(message.chat.id, "choose your balance", reply_markup=balance_forex4u4())
+    elif message.text == "AlpariMT5":
+        bot.send_message(message.chat.id, "choose your balance", reply_markup=balance_alpari5())
+    elif message.text == "Forex4uMT5":
+        bot.send_message(message.chat.id, "choose your balance", reply_markup=balance_forex4u5())
 
-
+######################################
+#
+#
+#
+##         MT4
+#
+#
+#
 @bot.callback_query_handler(func=lambda call: call.data.startswith("robo"))
 def handle_buy_callback(call):
     bot.send_message(call.message.chat.id, "waiting")
@@ -46,11 +63,23 @@ def handle_buy_callback(call):
     time.sleep(2)
     pyautogui.hotkey('enter')
     time.sleep(5)
-    pyautogui.click(x=22, y=45)
-    time.sleep(0.5)
-    pyautogui.click(x=22, y=187)
+    pyautogui.keyDown('alt')
+    pyautogui.press('f')
+    pyautogui.keyUp('alt')  # Optional
+    time.sleep(0.3)
+    pyautogui.press('down')
+    time.sleep(0.3)
+    pyautogui.press('down')
+    time.sleep(0.3)
+    pyautogui.press('down')
+    time.sleep(0.3)
+    pyautogui.press('down')
+    time.sleep(0.3)
+    pyautogui.press('down')
+    time.sleep(0.3)
+    pyautogui.hotkey('enter')
+    time.sleep(0.3)
 
-    time.sleep(0.5)
     pyautogui.hotkey('tab')
     time.sleep(0.5)
     pyautogui.hotkey('tab')
@@ -80,13 +109,13 @@ def handle_buy_callback(call):
     pyautogui.hotkey('tab')
     time.sleep(0.5)
     balance = "0"
-    if call.data == "robo5k":
+    if call.data == "roboMT45k":
         balance = "5000"
-    elif call.data == "robo10k":
+    elif call.data == "roboMT410k":
         balance = "10000"
-    elif call.data == "robo15k":
+    elif call.data == "roboMT415k":
         balance = "15000"
-    elif call.data == "robo25k":
+    elif call.data == "roboMT425k":
         balance = "25000"
     else:
         bot.send_message(call.message.chat.id, "Sorry I didn't understand")
@@ -150,7 +179,7 @@ def handle_buy_callback(call):
     pyautogui.keyUp('alt')  # Optional
 
 
-@bot.callback_query_handler(func=lambda call: call.data.startswith("alpari"))
+@bot.callback_query_handler(func=lambda call: call.data.startswith("alpariMT4"))
 def handle_buy_callback(call):
     pyautogui.hotkey('win')
     time.sleep(2)
@@ -192,13 +221,13 @@ def handle_buy_callback(call):
     pyautogui.hotkey('tab')
     time.sleep(0.5)
     balance = "0"
-    if call.data == "alpari5k":
+    if call.data == "alpariMT45k":
         balance = "5000"
-    elif call.data == "alpari10k":
+    elif call.data == "alpariMT410k":
         balance = "10000"
-    elif call.data == "alpari15k":
+    elif call.data == "alpariMT415k":
         balance = "15000"
-    elif call.data == "alpari25k":
+    elif call.data == "alpariMT425k":
         balance = "25000"
     else:
         bot.send_message(call.message.chat.id, "Sorry I didn't understand")
@@ -257,22 +286,17 @@ def handle_buy_callback(call):
                          f'broker with balance = {broker} \n username = {username} \n password = {password}',
                          reply_markup=get_main_buttons())
 
-
     pyautogui.keyDown('alt')
     pyautogui.press('f4')
     pyautogui.keyUp('alt')  # Optional
 
 
-
-
-
-
-@bot.callback_query_handler(func=lambda call: call.data.startswith("forex"))
+@bot.callback_query_handler(func=lambda call: call.data.startswith("forex4uMT4"))
 def handle_buy_callback(call):
     bot.send_message(call.message.chat.id, "waiting")
     pyautogui.hotkey('win')
     time.sleep(2)
-    pyautogui.write("Fo")
+    pyautogui.write("Forex4you")
     time.sleep(2)
     pyautogui.hotkey('enter')
     time.sleep(5)
@@ -308,13 +332,13 @@ def handle_buy_callback(call):
     pyautogui.hotkey('tab')
     time.sleep(0.5)
     balance = "0"
-    if call.data == "forex4u5k":
+    if call.data == "forex4uMT45k":
         balance = "5000"
-    elif call.data == "forex4u10k":
+    elif call.data == "forex4uMT410k":
         balance = "10000"
-    elif call.data == "forex4u15k":
+    elif call.data == "forex4uMT415k":
         balance = "15000"
-    elif call.data == "forex4u25k":
+    elif call.data == "forex4uMT425k":
         balance = "25000"
     else:
         bot.send_message(call.message.chat.id, "Sorry I didn't understand")
@@ -368,6 +392,144 @@ def handle_buy_callback(call):
     pyautogui.keyDown('alt')
     pyautogui.press('f4')
     pyautogui.keyUp('alt')  # Optional
+
+
+###################################################################
+#
+#
+#                MT5
+##
+#
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith("alpariMT5"))
+def handle_buy_callback(call):
+    pyautogui.hotkey('win')
+    time.sleep(2)
+    pyautogui.write("Alpari MT5")
+    time.sleep(2)
+    pyautogui.hotkey('enter')
+    time.sleep(5)
+    pyautogui.keyDown('alt')
+    pyautogui.press('f')
+    pyautogui.keyUp('alt')  # Optional
+    time.sleep(0.3)
+    pyautogui.press('down')
+    time.sleep(0.3)
+    pyautogui.press('down')
+    time.sleep(0.3)
+    pyautogui.press('down')
+    time.sleep(0.3)
+    pyautogui.press('down')
+    time.sleep(0.3)
+    pyautogui.press('down')
+    time.sleep(0.3)
+    pyautogui.hotkey('enter')
+    time.sleep(0.3)
+
+    pyautogui.hotkey('tab')
+    time.sleep(0.5)
+    pyautogui.hotkey('enter')
+    time.sleep(0.5)
+    pyautogui.hotkey('enter')
+    time.sleep(0.5)
+    random_name = random.choice(names)
+    pyautogui.write(random_name)
+    time.sleep(0.5)
+    pyautogui.hotkey('tab')
+    pyautogui.write(random_name)
+    time.sleep(0.5)
+    pyautogui.hotkey('tab')
+    time.sleep(0.5)
+    code = generate_random_codeMT5()
+    pyautogui.write(f'{random_name}.{code}@gmail.com')
+    time.sleep(0.5)
+    pyautogui.hotkey('tab')
+    time.sleep(0.5)
+    pyautogui.write("+1")
+    time.sleep(0.5)
+    pyautogui.hotkey('tab')
+    time.sleep(0.5)
+    pyautogui.write(code)
+    time.sleep(0.5)
+    pyautogui.hotkey('tab')
+    time.sleep(0.5)
+    pyautogui.hotkey('tab')
+    time.sleep(0.5)
+    pyautogui.press('down')
+    time.sleep(0.5)
+    pyautogui.hotkey('tab')
+    time.sleep(0.5)
+
+
+    balance = "0"
+    if call.data == "alpariMT55k":
+        balance = "5000"
+    elif call.data == "alpariMT510k":
+        balance = "10000"
+    elif call.data == "alpariMT515k":
+        balance = "15000"
+    elif call.data == "alpariMT525k":
+        balance = "25000"
+    else:
+        bot.send_message(call.message.chat.id, "Sorry I didn't understand")
+        return False
+
+    pyautogui.write(balance)
+    pyautogui.hotkey('tab')
+
+    time.sleep(0.5)
+    pyautogui.click(x=742, y=580)
+    time.sleep(0.5)
+    pyautogui.hotkey('tab')
+    time.sleep(0.5)
+
+    pyautogui.hotkey('tab')
+    time.sleep(0.5)
+    pyautogui.hotkey('enter')
+    time.sleep(4)
+    # pyautogui.hotkey('tab')
+    # time.sleep(0.5)
+    # pyautogui.hotkey('ctrl', 'c')
+    # password = clipboard.paste()
+    # print(password)
+    # time.sleep(0.5)
+    #
+    # pyautogui.hotkey('tab')
+    # time.sleep(0.5)
+    #
+    # pyautogui.hotkey('tab')
+    # time.sleep(0.5)
+    #
+    # pyautogui.hotkey('tab')
+    # time.sleep(0.5)
+    #
+    # pyautogui.hotkey('tab')
+    # time.sleep(0.5)
+    # pyautogui.hotkey('ctrl', 'c')
+    data = clipboard.paste()
+    print(data)
+    # time.sleep(0.5)
+    # pyautogui.hotkey('tab')
+    # time.sleep(0.5)
+    #
+    # pyautogui.hotkey('tab')
+    # time.sleep(0.5)
+    #
+    # pyautogui.hotkey('tab')
+    # time.sleep(0.5)
+    # pyautogui.hotkey('enter')
+    broker = call.data
+    if data is not None:
+        bot.send_message(call.message.chat.id,
+                         f'{data}',
+                         reply_markup=get_main_buttons())
+
+    pyautogui.keyDown('alt')
+    pyautogui.press('f4')
+    pyautogui.keyUp('alt')  # Optional
+
+
 
 
 
